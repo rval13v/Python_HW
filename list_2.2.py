c22101b8011e421023a32a98c8113d2a2a08847a
rval13v <livarfpv@gmail.com>
2025-05-09 16:23:49 +1100

    Update list_2.2.py 9.05.25

--- list_2.2.py
+++ list_2.2.py
@@ -1,24 +1,25 @@
+def sum_lists(longer, shorter):
+    result = [] # Создаем пустой список
+    for i in range(len(shorter)):  # Складываем элементы по индексам
+        result.append(shorter[i] + longer[i])
+
+    for i in range(len(shorter), len(longer)):  # Добавляем оставшиеся элементы длинного списка
+        result.append(longer[i])
+
+    return result
+
+def check_lists(numbers_1, numbers_2):
+    if len(numbers_1) > len(numbers_2):  # Сравниваем длину
+        longer = numbers_1
+        shorter = numbers_2
+    else:
+        longer = numbers_2
+        shorter = numbers_1
+
+    return sum_lists(longer, shorter)
+
 numbers_1 = [1, 2, 3, 4, 7, 4, 5]
 numbers_2 = [1, 2, 3, 3, 3, 7, 7, 6]
 
-if len(numbers_1) > len(numbers_2):  # Сравниваем длину
-    longer = numbers_1
-    shorter = numbers_2
-elif len(numbers_1) < len(numbers_2):
-    longer = numbers_2
-    shorter = numbers_1
-else:
-    longer = numbers_1  # Если длины равны.
-    shorter = numbers_2
-
-result = []
-
-def sum_lists():
-    for i in range(len(shorter)):  # Индексы по длине короткого списка
-        result.append(shorter[i] + longer[i])  # Складываем по индексам
-    for i in range(len(shorter), len(longer)):
-        result.append(longer[i])  # Добавляем остаток из длинного списка
-
-    print(result)
-
-sum_lists()
+result = check_lists(numbers_1, numbers_2)
+print(result)
