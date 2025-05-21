e72fd484c52613b72346d3049350274a8ac93f9a
rval13v <livarfpv@gmail.com>
2025-05-21 11:41:57 +1100

    Update Student.py

--- Student.py
+++ Student.py
@@ -2,40 +2,17 @@ def calculate_average(grades): # определение функции, кото
     if not grades:
         return 0
     return round(sum(grades) / len(grades), 2) # считаем среднее
+
      
-
-
 def add_student(name, grades): # функция создает студента по имени и списку баллов
     if not grades: # если баллы отсут, то raise
         print("Нужно ввести хотя бы один балл.")
         return
     return {"name": name, "grades": grades} # возвращает словарь, представляющий студента
 
-    while True: # бесконечный цикл ввожа оценок, пока не наступит break
-        grade_input = input(f"Балл {len(grades) + 1}: ").strip() # выводит запрос баллов по очереди
-        if grade_input == "": # если нажат пустой Enter
-            break
-        try:
-            grade = int(grade_input)
-            if 0 <= grade <= 100:   
-                grades.append(grade)
-            else:
-                print("Балл должен быть от 0 до 100.")
-        except ValueError:
-            print("Ошибка! Введите число.")
-
-    try:
-        students.append(add_student(input_text, grades))
-    except ValueError as e: # переменная в которую ловится Ошибка
-        print(f"Ошибка: {e}")
-    else:
-        student_reduction(students)
-
-
-    add_student(name, grades)  
 
 def student_average(students): # информ о студенте
-    print("\nТекущий список студентов:")
+    print("\nCписок студентов:")
     for student in students: # перебирает всех студентов в списке
         avg = calculate_average(student["grades"]) # считает Средний балл
         if avg >= 75:
@@ -43,8 +20,6 @@ def student_average(students): # информ о студенте
         else:
             status = "Неуспешен"
         print(f"\nСтудент: {student['name']}, \n\tСтатус: {status} \n\tСредний балл: {avg}")
-
-
 
 
 def student_reduction(students):
@@ -62,12 +37,7 @@ def overall_average(students): # общий Средний балл
     print(f"\nОбщий средний балл всех студентов: {all_average}")
 
 
-def update_list_students(students):
-    print("\nОбновлённый список студентов:")
-    for student in students:
-        print(f"\n{student['name']}, Баллы: {student['grades']}, Средний балл: {calculate_average(student['grades'])}")
-
-
+   
 students = [
     {"name": "Harry", "grades": [90, 58, 76]},
     {"name": "Hermione", "grades": [90, 85, 86]},
@@ -84,8 +54,27 @@ grades = [] # пустой список для баллов в который в
 
 print("Введите баллы (по одному). Чтобы закончить — нажмите Enter без ввода.")
 
+while True: # бесконечный цикл ввожа оценок, пока не наступит break
+    grade_input = input(f"Балл {len(grades) + 1}: ").strip() # выводит запрос баллов по очереди
+    if grade_input == "": # если нажат пустой Enter
+        break
+    try:
+        grade = int(grade_input)
+        if 0 <= grade <= 100:   
+            grades.append(grade)
+        else:
+            print("Балл должен быть от 0 до 100.")
+    except ValueError:
+        print("Ошибка! Введите число.")
+
+try:
+    students.append(add_student(input_text, grades))
+except ValueError as e: # переменная в которую ловится Ошибка
+    print(f"Ошибка: {e}")
+else:
+    student_reduction(students)
 
 
-
-update_list_students(students)
+student_average(students)
 overall_average(students)
+
