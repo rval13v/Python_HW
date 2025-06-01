01fc60217218c11886f40732ead47f8f3b9b5682
rval13v <livarfpv@gmail.com>
2025-06-01 20:43:02 +1100

    Update library.py

--- library.py
+++ library.py
@@ -12,13 +12,15 @@ def book_list_view(library):
 
 def add_book(title, author, year):
     if not title.strip() or not author.strip() or not year:
-        print("iguygitftfyg")
-        return
+        print("Вы ничего не ввели, добавление пропущено")
+        return False
 
     library[title] = {"author": author, "year": year, "availability": None}
     print(f"Книга '{title}' успешно добавлена.")
+    return True
+    
 
-def same_book(title):
+def check_book(title):
     if title in library:
         print(f"Предупреждение: Книга '{title}' уже присутствует в библиотеке.")
         choice = input("Обновить данные книги? Да/Нет [y/n]: ").strip().lower()
@@ -43,18 +45,22 @@ library = {
 
 }
 
+while True:
+    title = input("Введите название книги: ").strip().lower()
+    if not title:
+        break
 
-title = input("Введите название книги: ").strip().lower()
-
-
-if same_book(title):
-    author = input("Введите автора книги: ")
-    try:
-        year = int(input("Введите год издания книги: "))
-        add_book(title, author, year)
-    except ValueError:
-        print("Ошибка: год должен быть числом")
+    if check_book(title):
+        author = input("Введите автора книги: ").strip()
+        while not author:
+            author = input("Имя автора обязательно. Пожалуйста, повторите ввод: ").strip()
+            
+        try:
+            year = int(input("Введите год издания книги: "))
+            if add_book(title, author, year):
+                pass
+        except ValueError:
+            print("Ошибка: год должен быть числом")
 
 print("\nСписок книг в бибилиотеке: ")
 book_list_view(library)
-
