e3c9bb72531f31d8d4d599ca8428849fa283d3ea
rval13v <livarfpv@gmail.com>
2025-06-20 17:24:37 +1100

    Update and rename test_Task_Meneger(unuttest.py to test_Task_Meneger(unuttest).py

--- test_Task_Meneger(unuttest).py
+++ test_Task_Meneger(unuttest).py
@@ -17,12 +17,21 @@ class TestTaskManager(unittest.TestCase):
         todo_list = TaskManager()
         todo_list.add_task("Купить пиво")         
         self.assertEqual(len(todo_list.tasks), 1)  # Проверяем, что задача добавлена
-        
-        
-        todo_list.add_task(0)
         self.assertIn("Купить пиво", [task['description'] for task in todo_list.tasks])
         
-)
+    def test_complete_task(self):
+        todo_list = TaskManager()
+        todo_list.add_task("Купить пиво") 
+        self.assertTrue(todo_list.complete_task)  
+
+    def test_save_to_json(self):
+        test_data = {"Купить продукты"}  
+        filename = "tasks.json.txt"
+
+        
+ 
+
+
         
 if __name__ == '__main__':
     unittest.main()
